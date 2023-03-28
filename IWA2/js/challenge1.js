71a9da490ee4060bbf43952713ec23e63039f2b0
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-03-28 13:26:24 +0200

    anotherIWAAdded

---
+++ IWA2/js/challenge1.js
@@ -0,0 +1,19 @@
+// This is the firstname of the user 
+
+const user = 'John'
+
+
+//  This is the lastname of the user 
+const surname = 'Smith'
+
+// This console displays the user and surname declaration
+console.log(user, surname)
+
+
+/* This code had a error comments that made the whole code a comment. 
+    - It also had a semi colon to the console
+    
+   
+     // An alternative way to debug the first name and the last name could have been like: console.log("John Smith")
+
+*/
---
+++ IWA2/js/challenge3.js
@@ -0,0 +1,15 @@
+/*
+It is important to show the following message in order to:
+- to assure users
+- to scare hackers
+- to impress investors
+*/
+
+console.warn("Security scan starting")
+
+/* It is important to let user know when they can close the page  */
+
+console.info("Please wait for scan to complete before closing the browser.")
+
+
+/* Instead of multiple comments, inline comment was used and there was no inverted commas for the console.warn and also for the info console */
