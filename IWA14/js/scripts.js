3ed3a2f91002d05db36e5aec9de58c6994b81298
OratileMokgoatlheng <mokgoatlhengo@gmail.com>
2023-04-18 13:47:02 +0200

    IWA14Added

---
+++ IWA14/js/scripts.js
@@ -0,0 +1,16 @@
+const firstName = 'John';            //used const declaration
+const age = 35;                      //used const declaration
+const hobbie = 'Coding';             //used const declaration and changed the variable
+
+const logTwice = () => {
+  console.log(parameter)
+  console.log(parameter)
+}
+
+function hobby () {
+  
+  console.log(`Hello, ${firstName} (${age}). I love ${hobbie}!`)   //removed the funtion and consoled the interpolation
+}
+
+hobby()
+hobby()          //called the hobby funtion again
